5380309e109b0813a530ddb78246e313f8357f92
AlejandroFV <AlejandroFV@mail.com>
2014-11-19 22:27:09 -0600

    Revert "Revert "Fields encapsulated""
    This reverts commit 92a79d2a321cb5a3f31a1360ee053eb8821eed95.

--- SAC/src/Modelo/Optometrista.java
+++ SAC/src/Modelo/Optometrista.java
@@ -6,10 +6,30 @@
 
 package modelo;
 
+import controlador.ControladorPacientes;
+
 /**
  *
  * @author Asus
  */
 public class Optometrista {
     
+    public ControladorPacientes pacientes;
+
+    public Optometrista() {
+    }
+
+    /**
+     * @return the pacientes
+     */
+    public ControladorPacientes getPacientes() {
+        return pacientes;
+    }
+
+    /**
+     * @param pacientes the pacientes to set
+     */
+    public void setPacientes(ControladorPacientes pacientes) {
+        this.pacientes = pacientes;
+    }
 }
--- SAC/src/Modelo/Vendedor.java
+++ SAC/src/Modelo/Vendedor.java
@@ -6,10 +6,46 @@
 
 package modelo;
 
+import controlador.ControladorDeudas;
+import controlador.ControladorVentas;
+
 /**
  *
  * @author Asus
  */
 public class Vendedor {
     
+    public ControladorDeudas deudas;
+    public ControladorVentas ventas;
+
+    public Vendedor() {
+    }
+
+    /**
+     * @return the deudas
+     */
+    public ControladorDeudas getDeudas() {
+        return deudas;
+    }
+
+    /**
+     * @param deudas the deudas to set
+     */
+    public void setDeudas(ControladorDeudas deudas) {
+        this.deudas = deudas;
+    }
+
+    /**
+     * @return the ventas
+     */
+    public ControladorVentas getVentas() {
+        return ventas;
+    }
+
+    /**
+     * @param ventas the ventas to set
+     */
+    public void setVentas(ControladorVentas ventas) {
+        this.ventas = ventas;
+    }
 }
